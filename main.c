c7f3bd6f299712e3d5cd9a24456e90b66fc37e59
ruthosoro <robinahruth@gmail.com>
2023-12-08 05:30:53 -0800

    first

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main: starting point of our program
+ * argc: number of arguments in a command line
+ * argv: the array of pointers to the arguments in the command line
+ * return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int a = 1;
+		char *line = NULL;
+		char *input = argv[0];
+		ssize_t geterror = 0;
+		size_t result = 0;
+		 
+
+		while (a <= 1)
+		{
+		line = NULL 
+		geterror = getline(&line, result, stdin);
+		if (geterror <= 1)
+			{
+				free(line);
+				exit(0);
+			}
+		input_controller(&line, input);
+		free(line);
+		}
+		return (0);
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifdef MAIN_H
+#define MAIN_H
+#include <stdin.h>
+#include <stdio.h>
+#include <errno.h>
+
+#endif
