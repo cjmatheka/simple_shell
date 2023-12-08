b36e6cea52ef34a2b5925c71e8522f9631a14542
Julius Charles <cjmatheka@gmail.com>
2023-12-08 16:01:42 +0000

    Update main.h file with required c libraries

--- main.h
+++ main.h
@@ -1,7 +1,18 @@
 #ifdef MAIN_H
 #define MAIN_H
+
+/* The library/header files are included here */
 #include <stdin.h>
-#include <stdio.h>
 #include <errno.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+
+/* The function propotypes are included here */
 
 #endif
