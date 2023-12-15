e56ce36445460b280f0e2b569e5801ce08440147
Julius Charles <cjmatheka@gmail.com>
2023-12-15 20:19:55 +0300

    handle exit functionality

--- ex_cmd.c
+++ ex_cmd.c
@@ -7,59 +7,56 @@
 
 void exeCmds(char **commands)
 {
-	pid_t child_pid;
-	int i;
+    int i;
 
-	for (i = 0; commands[i] != NULL; i++)
+    for (i = 0; commands[i] != NULL; i++)
+    {
+        if (_strcmp(commands[i], "ls") == 0)
 	{
-		child_pid = fork();
+            _ls(".");
+        }
+	else if (_strcmp(commands[i], "cd") == 0)
+	{
+            if (i > 1)
+	    {
+                _cd(commands[1]);
+            } else {
+                _cd(".");
+            }
+        }
+	else if (_strcmp(commands[i], "pwd") == 0)
+	{
+            _pwd();
+        }
+	else if (_strcmp(commands[i], "clear") == 0)
+	{
+            clearTerminal();
+        } else if (_strcmp(commands[i], "exit") == 0 || _strcmp(commands[i], "quit") == 0)
+	{
+            exit(0);
+        }
+	else
+	{
+            pid_t child_pid = fork();
 
-		if (child_pid == 0)
-		{
-			/* This code runs in the child process */
-			if (_strcmp(commands[i], "ls") == 0)
-			{
-				_ls(".");
-			}
-			else if (_strcmp(commands[i], "cd") == 0)
-			{
-				if (i > 1)
-				{
-					_cd(commands[1]);
-				}
-				else
-				{
-					_cd(".");
-				}
-			}
-			else if (_strcmp(commands[i], "pwd") == 0)
-			{
-				_pwd();
-			}
-			else if (_strcmp(commands[i], "clear") == 0)
-			{
-				clearTerminal();
-			}
-			else if (_strcmp(commands[i], "exit") == 0)
-			{
-				_exitShell(0);
-			}
-			else
-			{
-				execve(commands[i], commands, environ);
-				perror("execve"); /* If execve fails */
-				exit(EXIT_FAILURE);  /* Exit child process if execve fails */
-			}
-		}
-		else if (child_pid < 0)
-		{
-			perror("fork");
-		}
-		else
-		{
-			waitHelper(child_pid); /* waits for child, runs in the parent process */
-		}
-	}
+            if (child_pid == 0)
+	    {
+                /* Child process */
+                execve(commands[i], commands, environ);
+                perror("execve"); /* If execve fails */
+                exit(EXIT_FAILURE);
+            }
+	    else if (child_pid < 0)
+	    {
+                perror("fork");
+            }
+	    else
+	    {
+                /* Parent process */
+                waitHelper(child_pid); /* waits for child */
+            }
+        }
+    }
 }
 
 
